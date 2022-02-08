130f86e8f74c782f2105284176179252443b4385
AntCasson <anthonyrcasson@gmail.com>
2022-02-08 23:08:48 +0100

    1 binaries uploaded

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,24 +1,27 @@
 import { useTimer } from 'use-timer';
 import style from "./style/Timer.sass"
 import Button from '@mui/material/Button';
-import ButtonGroup from '@mui/material/ButtonGroup';
 
 
 
 export default function Timer() {
-  const { time, start, pause, reset, status } = useTimer();
+  const { time, start, pause, reset, status } = useTimer({
+    endTime: 0,
+    initialTime: 180,
+    timerType: 'DECREMENTAL',
+  });
 
   return (
     <>
-      <div className="wrapper">
-        <ButtonGroup color="secondary" variant="contained" aria-label="outlined primary button group">
-          <Button onClick={start}>Start</Button>
-          <Button onClick={pause}>Pause</Button>
-          <Button onClick={reset}>Reset</Button>
-        </ButtonGroup>
+      <div className="button-wrapper">
+        
+          <Button color="secondary" variant="contained" onClick={start}>Start</Button>
+          <Button color="secondary" variant="outlined" onClick={pause}>Pause</Button>
+          <Button color="secondary" variant="outlined" onClick={reset}>Reset</Button>
+        
       </div>
       <p>Elapsed time: {time}</p>
-      {status === 'RUNNING' && <p>Running...</p>}
+      {status === 'RUNNING' && <p>Running...</p> || status === 'STOPPED' && <p>drink!</p>}
     </>
   );
 };
