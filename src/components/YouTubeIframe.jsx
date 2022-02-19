197f13f033713184f513347907af636adf00f2db
AntCasson <anthonyrcasson@gmail.com>
2022-02-19 21:15:37 +0100

    added serviceworker and hd logo

--- src/components/YouTubeIframe.jsx
+++ src/components/YouTubeIframe.jsx
@@ -5,7 +5,7 @@ export default function YoutubeIframe({ url, showAeropress }) {
       title='YouTube video player'
       frameBorder='0'
       allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-      allowFullscreen
+      allowFullScreen
     ></iframe>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/sw.js")
+    .then(reg => console.log("sw is registered!", reg))
+    .catch(err => console.log("sw is not registered!", err));
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
